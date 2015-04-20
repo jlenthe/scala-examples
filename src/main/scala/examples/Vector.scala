5fedfff6e07f0abac30e5bb6185342323b49a145
Jason Lenthe <lenthe@comcast.net>
2015-04-19 22:06:12 -0400

    Initial population from eclipse project.

---
+++ src/main/scala/examples/Vector.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2015 Jason Lenthe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package examples
+
+/** An immutable double-precision math vector of any dimension 
+  *  
+  * @constructor Creates the vector using the provided Scala library Vector
+  */
+class Vector(_data: scala.collection.immutable.Vector[Double]) {
+    
+    /** Creates the vector using the provided Doubles as the vector elements. 
+      * @param d Variable number of double arguments to place in the vector
+      */
+    def this(d: Double*) = this(d.toVector)
+
+    /** The raw elements of this Vector */
+    def data = _data
+
+    /** Invokes the provided function on corresponding pairs of elements from this Vector
+      * and the Vector argument (that).
+      * 
+      * @param that the other vector to map elements to.  It must contain the same number of elements are this
+      * Vector or a Left[Exception] is returned.
+      * 
+      * @return Either the Vector that results when f is invoked on corresponding pairs of Doubles from this
+      * and that or an Exception describing the error that occurred.
+      */
+    def map2(that: Vector)(f: (Double, Double) => Double): Either[Exception, Vector] =
+        if (Vector.this.data.length == that.data.length)
+            Right(new Vector(((Vector.this.data, that.data)).zipped.map(f)))
+        else
+            Left(new Exception("Vector operation performed mismatching vector length. First length: %d. Second length: %d".format(
+                    this.data.length,
+                    that.data.length)))
+
+    /** Invokes the provided function on every element of the Vector producing a new Vector 
+      *
+      * @param f The function that is invoked on each element of this Vector
+      * 
+      * @return A new Vector containing the results of all the function invocations  
+      */
+    def map(f: Double => Double): Vector = new Vector(Vector.this.data.map(f))
+
+    /** Reduces the vector using a reduction function.
+      * 
+      * @param f The reduction function
+      * 
+      * @return The accumulated result
+      */
+    def reduce(f: (Double, Double) => Double): Double = Vector.this.data.reduce(f)
+
+    /** Gets the ith vector element
+      * 
+      * @param i The index of the Vector element to get
+      */
+    def apply(i: Int): Double = Vector.this.data.apply(i)
+
+    /** Computes the [[https://en.wikipedia.org/wiki/Dot_product dot]] product of this Vector 
+      *  and the provided Vector
+      *
+      * @param v The other vector in the dot product operation.  It must contain the same 
+      * number of elements are this Vector or a Left[Exception] is returned.
+      * 
+      * @return Either the dot product of this and v or an Exception describing
+      * the error that occurred.
+      */
+    def dot(v: Vector): Either[Exception,Double] = map2(v)(_ * _).right.map(_.reduce(_ + _))
+
+    def magnitude(): Double = Math.sqrt(Vector.this.map(Math.pow(_, 2.0)).reduce(_ + _))
+
+    def unitize(): Vector = Vector.this / Vector.this.magnitude
+
+    def +(v: Vector): Either[Exception, Vector] = Vector.this.map2(v)(_ + _)
+
+    def -(v: Vector): Either[Exception, Vector] = Vector.this.map2(v)(_ - _)
+
+    def *(v: Vector): Either[Exception, Vector] = Vector.this.map2(v)(_ * _)
+
+    def *(s: Double): Vector = Vector.this.map(_ * s)
+
+    def /(s: Double): Vector = Vector.this.map(_ / s)
+
+    def length = Vector.this.data.length
+
+    def cross(that: Vector): Either[Exception, Vector] =
+        if (Vector.this.length == 3 && that.length == 3)
+            Right(new Vector(this(1) * that(2) - this(2) * that(1), 
+                            this(2) * that(0) - this(0) * that(2),
+                            this(0) * that(1) - this(1) * that(0)))
+        else
+            Left(new Exception("Both Vecs must be 3 element for cross product."))
+
+    override def equals(a: Any) = 
+        a match {
+            case v: Vector => Vector.this.data == v.data
+            case _ => false
+        }
+    
+    override def hashCode() = Vector.this.data.hashCode()
+            
+    override def toString =
+        this.data.tail.fold("Vector [" + _data.head.toString)((s, d) => s + ", " + d.toString) + "]"
+}
---
+++ src/test/scala/examples/TestVector.scala
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2015 Jason Lenthe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package examples
+
+import org.junit._
+import org.junit.Assert._
+
+class TestVector {
+    @Test
+    def testConstructor() = {
+        val x = 1.0
+        val y = 2.0
+        val z = 3.0
+
+        val v = new Vector(x, y, z)
+
+        assertEquals(x, v(0), 0.0)
+        assertEquals(y, v(1), 0.0)
+        assertEquals(z, v(2), 0.0)
+    }
+    
+    @Test
+    def testEquals() = {
+        val u = new Vector(1.0, 2.0, 3.0)
+        val v = new Vector(1.0, 2.0, 3.0)
+        
+        assertEquals(u, v)
+    }
+    
+    @Test
+    def testToString() = {
+        val v = new Vector(1.0, 2.0, 3.0)
+        val s = v.toString()
+        
+        assertEquals("Vector [1.0, 2.0, 3.0]", s)
+    }
+    
+    @Test
+    def testMap2() = {
+        val u = new Vector(2.0, 3.0, 4.0);
+        val v = new Vector(3.0, 4.0, 5.0);
+        
+        val w = u.map2(v)((x, y) => x + y);
+        
+        w match {
+            case Right(x) => assertEquals(new Vector(5.0, 7.0, 9.0), x)
+            case Left(error) => fail()
+        }
+    }
+    
+    @Test
+    def testMap2WrongSize() = {
+        val u = new Vector(2.0, 3.0, 4.0);
+        val v = new Vector(3.0, 4.0);
+        
+        val w = u.map2(v)((x, y) => x + y);
+        
+        val expectedError = "Vector operation performed mismatching vector length. First length: 3. Second length: 2"
+        
+        w match {
+            case Right(x) => fail()
+            case Left(exception) => assertEquals(expectedError, exception.getMessage())
+        }
+    }
+    
+    @Test
+    def testMap() = {
+        val u = new Vector(2.0, 3.0)
+        val v = u.map(x => x * x)
+        
+        assertEquals(new Vector(4.0, 9.0), v)
+    }
+    
+    @Test
+    def testReduce() = {
+        val v = new Vector(1.0, 2.0, 3.0)
+        
+        val r = v.reduce((x, y) => x + y)
+        
+        assertEquals(6.0, r, 0.0)
+    }
+    
+    @Test
+    def testDot() = {
+        val v = new Vector(1.0, 2.0, 3.0)
+        
+        val result = v.dot(v)
+        
+        result match {
+            case Right(d) => assertEquals(14.0, d, 0.0)
+            case Left(error) => fail(error.getMessage())
+        }   
+    }
+    
+    @Test
+    def testMagnitude() = {
+        val v = new Vector(3.0, 4.0)
+        
+        val magnitude = v.magnitude
+        
+        assertEquals(5.0, magnitude, 0.0)
+    }
+    
+    @Test
+    def testUnitize() = {
+        val v = new Vector(4.0, 5.0)
+        
+        val u = v.unitize()
+        
+        assertEquals(0.625, u(0), 1e-3)
+        assertEquals(0.781, u(1), 1e-3)
+    }
+    
+    @Test
+    def testUnitizeZeros() = {
+        val v = new Vector(0.0, 0.0)
+        
+        val u = v.unitize()
+        
+        assertTrue(u(0).isNaN)
+        assertTrue(u(1).isNaN)
+    }
+    
+    @Test
+    def testHashCodeDifferent() = {
+        val u = new Vector(1.0, 2.0)
+        val v = new Vector(2.0, 3.0)
+        
+        assertNotEquals(u.hashCode(), v.hashCode())
+    }
+    
+    @Test
+    def testHashCodeSame() = {
+        val u = new Vector(1.0, 2.0)
+        val v = new Vector(1.0, 2.0)
+        
+        assertEquals(u.hashCode(), v.hashCode())
+    }
+    
+    @Test
+    def testPlus() = {
+        val u = new Vector(1.0, 2.0)
+        val v = new Vector(3.0, 4.0)
+        
+        val result = u + v
+        
+        result match {
+            case Left(exception) => fail()
+            case Right(sum) => assertEquals(new Vector(4.0, 6.0), sum)
+        }
+    }
+    
+    @Test
+    def testPlusWrongSize() = {
+        testOperationOnWrongSizeVectors((a: Vector, b: Vector) => a + b)
+    }
+    
+    @Test
+    def testMinus() = {
+        val u = new Vector(1.0, 2.0)
+        val v = new Vector(3.0, 4.0)
+        
+        val result = v - u
+        
+        result match {
+            case Left(exception) => fail()
+            case Right(sum) => assertEquals(new Vector(2.0, 2.0), sum)
+        }
+    }
+    
+    @Test
+    def testMinusWrongSize() = {
+        testOperationOnWrongSizeVectors((a: Vector, b: Vector) => a - b)
+    }
+    
+    @Test
+    def testVectorMultiply() = {
+        val u = new Vector(1.0, 2.0)
+        val v = new Vector(3.0, 4.0)
+        
+        val result = v * u
+        
+        result match {
+            case Left(exception) => fail()
+            case Right(sum) => assertEquals(new Vector(3.0, 8.0), sum)
+        }
+    }
+    
+    @Test
+    def testVectorMultiplyWrongSize() = {
+        testOperationOnWrongSizeVectors((a: Vector, b: Vector) => a * b)
+    }
+    
+    def testOperationOnWrongSizeVectors(f: (Vector, Vector) => Either[Exception,Vector]) = {
+        val u = new Vector(2.0, 3.0, 4.0);
+        val v = new Vector(3.0, 4.0);
+        
+        val w = f(u, v)
+        
+        val expectedError = "Vector operation performed mismatching vector length. First length: 3. Second length: 2"
+        
+        w match {
+            case Right(x) => fail()
+            case Left(exception) => assertEquals(expectedError, exception.getMessage())
+        }
+    }
+    
+    def testScalarMultiply() = {
+        val v = new Vector(2.0, 3.0)
+        
+        val w = v * 3.0
+        
+        assertEquals(new Vector(6.0, 9.0), w)
+    }
+    
+    def testScalaraDivide() = {
+        val v = new Vector(4.0, 8.0)
+        
+        val w = v / 2.0
+        
+        assertEquals(new Vector(2.0, 4.0), w)
+    }
+}
