1f08256bf7a4c1676f1dcee5fb4c49c6dde7a3fd
Jason Lenthe <lenthe@comcast.net>
2016-05-21 16:43:34 -0400

    Added RootFinder scaladoc comments.

--- src/main/scala/examples/BisectionRootFinder.scala
+++ src/main/scala/examples/BisectionRootFinder.scala
@@ -2,10 +2,29 @@ package examples
 
 import scala.math._
 
+/** Interface for root finding objects */
 trait RootFinder {
+  
+    /** Finds the roots of f bracketed by points a and b.
+      *  
+      * Points a and b bracket a root when signum(f(a) != signum(f(b)).  
+      *  
+      * @param f The function for which a root will be found
+      * 
+      * @param a The first bracket point
+      * 
+      * @param b The second bracket point
+      *  
+      * @return Either the approximate root of the function contained with the (a, b) bracket (Right) 
+      * or an error message string (Left)
+      */
     def apply(f: Double => Double, a: Double, b: Double): Either[String,Double]
 }
 
+/** Root finder that use the bisection algorithm  
+  * 
+  * @param tol The tolerance to which to find roots
+  */
 class BisectionRootFinder (tol: Double) extends RootFinder {
   
   override def apply(f: Double => Double, a: Double, b: Double): Either[String,Double] = {
